33c422f99a87792b0ef1e9561dfaf19bb2cea6a5
vivek-kumar85 <vivekshriwas12911@gmail.com>
2024-01-31 23:55:18 +0530

    Fixed Html template

--- main.py
+++ main.py
@@ -5,19 +5,21 @@ from fastapi.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
 from app.routers import csv_handler, logging_handler
 
+
 app = FastAPI()
-
-# Include your routers
-app.include_router(csv_handler.router, prefix="/csv", tags=["csv"])
-app.include_router(logging_handler.router, prefix="/logging", tags=["logging"])
-
-# Mount the static files directory to serve HTML
-app.mount("/", StaticFiles(directory="static"), name="static")
 
 # Serve the HTML file at the root endpoint
 @app.get("/")
 def read_root():
     return FileResponse("static/index.html")
+
+# Include your routers
+app.include_router(csv_handler.router, prefix="/csv", tags=["csv"])
+app.include_router(logging_handler.router, prefix="/logging", tags=["logging"])
+
+
+# Mount the static files directory to serve HTML
+app.mount("/", StaticFiles(directory="static"), name="static")
 
 
 if __name__ == "__main__":
